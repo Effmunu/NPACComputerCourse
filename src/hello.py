366c29fc5a1c9373bbe338131e9df2415a43c1b3
npac09 <laudrain@ipno.in2p3.fr>
2016-02-11 15:57:59 +0100

    Step 2. Creating hello.py

---
+++ src/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Print a conventional message
+
+:Author: LAL ens <ens@lal.in2p3.fr>
+:Date:   February 2016
+
+First program hello.py: display "Hello, world!" on standard output.
+"""
+
+print "Hello, world!"
