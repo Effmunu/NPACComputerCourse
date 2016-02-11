5f8ef92a54a53edb6939fddf67458f5d8eb3aa8a
npac09 <laudrain@ipno.in2p3.fr>
2016-02-11 16:10:52 +0100

    Step 3.

--- src/hello.py
+++ src/hello.py
@@ -1,12 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Print a conventional message
+"""Print message 'Hello, world!'.
 
-:Author: LAL ens <ens@lal.in2p3.fr>
+:Author: LAL npacxx <npacxx@lal.in2p3.fr>
 :Date:   February 2016
-
-First program hello.py: display "Hello, world!" on standard output.
 """
 
-print "Hello, world!"
+import sys
+
+# By convention, a function name must contain only lowercase characters and _.
+# string=None defines a default value for string and makes it optional.
+def print_msg(string=None):
+    """Print a message received as an argument or a default message
+    if none is passed.
+
+    :param string: a string to print (optional)
+    :return: status value (always success, 0)
+    """
+
+    if string is None:
+        # Define a default message
+        string = 'Hello, world!'
+    print "%s" % string
+    return 0
+
+# The following test is considered as a best practice: this way a module
+# can be used both as a standalone application or as a module called by another
+# module.
+if __name__ == "__main__":
+
+    # The main program is implement mainly as a function: this avoids having
+    # all the variables used in this context (e.g. string in print_msg) to
+    # become global variables.
+    status = print_msg()
+
+    sys.exit(status)
