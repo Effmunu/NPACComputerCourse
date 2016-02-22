0d61c5aba8f7b5c0463cf03302974e11a5b1ef9e
npac09 <laudrain@ipno.in2p3.fr>
2016-02-22 15:52:11 +0100

    Adding methods to get centroid coords and centroid wcs coords. + small corrections (Pylint)

--- src/Cluster.py
+++ src/Cluster.py
@@ -7,7 +7,11 @@ This module defines the cluster class
 
 import library
 
-class Cluster:
+# pylint: disable=R0902
+# We have decided to use four parameters for xmin, xmax, ymin, ymax of the bounding box
+# for the sake of clarity. Therefore, we don't want to reduce the number of attributes (only 9)
+
+class Cluster(object):
     """
     This class is used to gather basic information on a cluster found :
     :attribute pixel_list: the list of pixels (doublet of integers)
@@ -26,7 +30,7 @@ class Cluster:
         self.pixel_list = []
         self.integral = 0.
         # Initialization of the coordinates (centroid and bounding box)
-        # with the first pixel (WARNING : we get (x,y) by taking (row, col))
+        # with the first pixel (WARNING : we get (y,x) by taking (row, col))
         self.box_ymin, self.box_xmin = pixel_list[0]
         self.box_ymax, self.box_xmax = pixel_list[0]
         self.centroid = pixel_list[0]
@@ -46,10 +50,23 @@ class Cluster:
                 self.box_ymin = row
 
         # find the centroid
-        self.centroid = (self.box_xmax + self.box_xmin) / 2., \
+        self.centroid = self.find_centroid()
+
+        # create a WCS object to make conversions
+        self.my_wcs = library.WCS(header)
+
+        # convert to WCS coordinates
+        self.centroid_wcs = self.convert_centroid_to_wcs()
+
+    def find_centroid(self):
+        """
+        :return: centroid coordinates (x,y)
+        """
+        return (self.box_xmax + self.box_xmin) / 2., \
                         (self.box_ymax + self.box_ymin) / 2.
 
-        # create a WCS object
-        my_wcs = library.WCS(header)
-        # convert to WCS coordinates
-        self.centroid_wcs = my_wcs.convert_to_radec(self.centroid[0], self.centroid[1])
+    def convert_centroid_to_wcs(self):
+        """
+        :return: centroid wcs coordinates (ra, dec)
+        """
+        return self.my_wcs.convert_to_radec(self.centroid[0], self.centroid[1])
