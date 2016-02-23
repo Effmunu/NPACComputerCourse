4932abb3082b11b242b6d432cb5b30c8ba43bb6b
npac09 <laudrain@ipno.in2p3.fr>
2016-02-23 16:55:59 +0100

    Check docstrings

--- src/mylib.py
+++ src/mylib.py
@@ -24,7 +24,6 @@ def open_fits(input_file_path):
     """
     try:
         with fits.open(input_file_path) as data_blocks:
-            data_blocks.info()
             return data_blocks[0].header, data_blocks[0].data
 
     except IOError:
@@ -36,17 +35,20 @@ def open_fits(input_file_path):
 ##########
 def gaussian(x, amplitude, mean, sigma):
     """
-    compute a gaussian function:
+    :param x: variable
+    :param amplitude: amplitude of the gaussian
+    :param mean: mean of the gaussian
+    :param sigma: dispersion of the gaussian
+    :return: value of a gaussian with the above parameters, at x
     """
     return amplitude * np.exp(- (x - mean) * (x - mean) / (2 * sigma * sigma))
 
 def gaussian_fit(xdata, ydata):
     """
     Fit a set of data with a gaussian function.
-    :param xdata: bin boundaries values
-    :param ydata: bin contents (values)
-    :return:    The fit parameters array 'fit_param',
-                and the covariant matrix 'covariant'.
+    :param xdata: bin boundaries values (x values)
+    :param ydata: bin contents (values) (y values)
+    :return:      The gaussian fit parameters: amplitude, background, dispersion
     """
 
     # normalization parameters
@@ -54,9 +56,9 @@ def gaussian_fit(xdata, ydata):
     m_y = np.float(np.max(ydata))
 
     # apply the fit, normalize the data to help the fitting program (no need for covariant matrix)
-    fit_param, _ = curve_fit(gaussian, \
-                       xdata / m_x, \
-                       ydata / m_y)
+    fit_param, _ = curve_fit(gaussian,
+                             xdata / m_x,
+                             ydata / m_y)
 
     # get back the un-normalized parameters
     amplitude = fit_param[0] * m_y
@@ -69,7 +71,7 @@ def remove_background(pixels, background, threshold):
     """
     :param pixels: original image matrix
     :param background: mean value of the gaussian fit on the pixel distribution
-    :param threshold:
+    :param threshold: threshold to be applied for background removal
     :return: an image, similar to 'pixels', with the background removed
     """
     mask = pixels >= background + threshold
@@ -81,14 +83,14 @@ def remove_background(pixels, background, threshold):
 ##########
 def explore_cluster(pixels_visited, pixels, row, col, threshold):
     """
+    Recursive function.
     The image is assumed to be a matrix
-    with first coordinate going from top to bottom
-    and second coordinate going from right to left
-
+    with first coordinate going from right to left
+    and second coordinate going from top to bottom
     :param pixels_visited: boolean matrix of visited pixels (1 = visited)
     :param pixels: original image matrix
-    :param row: x coordinate of the seed to begin the search
-    :param col: y coordinate of the seed to begin the search
+    :param row: y coordinate of the seed to begin the search
+    :param col: x coordinate of the seed to begin the search
     :param threshold: detection threshold, usually "mean + 6*sigma"
     :return: the list of pixels in the cluster
     """
@@ -116,7 +118,7 @@ def find_clusters(header, pixels, threshold):
     Find all the clusters in a FITS image, given a threshold
     :param header: header of the FITS image
     :param pixels: original image matrix
-    :param threshold:
+    :param threshold: threshold to be applied
     :return: list of clusters in the image, dictionary of clusters indexed by centroid coordinates
     """
     # We define an array of pixels visited: 1 if visited, 0 elsewise
@@ -143,7 +145,7 @@ def build_cluster_dico(cluster_list, search_radius=0.003):
     Build a dictionary of clusters
     :param header: header of the FITS image
     :param pixels: original image matrix
-    :param threshold:
+    :param threshold: threshold to be applied
     :return: dictionary of clusters: key = centroid coords, values = [cluster object, name]
     """
     cluster_dico = {}
